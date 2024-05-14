34d15873ded6ffd9ef694da481dfcc9d4e6f7ce4
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-05-14 14:23:01 +0200

    Some renames

--- src/main/java/kea/bowlingBackend/bowlingBackendApplication.java
+++ src/main/java/kea/bowlingBackend/bowlingBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KinobackendApplication {
+public class bowlingBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KinobackendApplication.class, args);
+        SpringApplication.run(kea.kinoBackend.bowlingBackendApplication.class, args);
     }
 
 }
